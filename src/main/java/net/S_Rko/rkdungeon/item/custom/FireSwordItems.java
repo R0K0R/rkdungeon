e4edd2ace007e16bdfe1736fddc8912afeedf403
injoy <rkr0k0r@gmail.com>
2022-11-05 23:11:42 +0900

    added Fire Sword function

--- src/main/java/net/S_Rko/rkdungeon/item/custom/FireSwordItems.java
+++ src/main/java/net/S_Rko/rkdungeon/item/custom/FireSwordItems.java
@@ -1,11 +1,23 @@
 package net.S_Rko.rkdungeon.item.custom;
 
 import net.S_Rko.rkdungeon.item.ModItems;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class FireSwordItems extends SwordItem {
     public FireSwordItems(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
@@ -18,6 +30,26 @@ public class FireSwordItems extends SwordItem {
 
     public static boolean isUsable(ItemStack stack) {
         return stack.getDamage() < stack.getMaxDamage() - 1;
+    }
+
+    public boolean canMine(BlockState state, World world, BlockPos pos, PlayerEntity miner) {
+        return false;
+    }
+
+    public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
+        user.playSound(SoundEvents.ITEM_FIRECHARGE_USE, 1, 1);
+        entity.setFireTicks(10000000);
+        return ActionResult.PASS;
+    }
+
+    public int getMaxUseTime(ItemStack stack) {
+        return 1;
+    }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        user.setCurrentHand(hand);
+        return TypedActionResult.consume(itemStack);
     }
 
     public boolean canRepair(ItemStack stack, ItemStack ingredient) {
