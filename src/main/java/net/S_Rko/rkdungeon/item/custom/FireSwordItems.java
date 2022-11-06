82c4c3c0433b0620070dce80dd829b9284324a94
injoy <rkr0k0r@gmail.com>
2022-11-06 19:00:53 +0900

    updated fire sword features

--- src/main/java/net/S_Rko/rkdungeon/item/custom/FireSwordItems.java
+++ src/main/java/net/S_Rko/rkdungeon/item/custom/FireSwordItems.java
@@ -1,25 +1,39 @@
 package net.S_Rko.rkdungeon.item.custom;
 
 import net.S_Rko.rkdungeon.item.ModItems;
-import net.minecraft.block.BlockState;
+import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.block.*;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.SwordItem;
-import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.*;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 
 public class FireSwordItems extends SwordItem {
+
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        stack.damage(1, attacker, (e) -> {
+            e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
+        });
+        target.setFireTicks(200);
+        return true;
+    }
+
+    int DurabilityCount;
+
     public FireSwordItems(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
     }
@@ -39,7 +53,60 @@ public class FireSwordItems extends SwordItem {
     public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
         user.playSound(SoundEvents.ITEM_FIRECHARGE_USE, 1, 1);
         entity.setFireTicks(10000000);
+        if (DurabilityCount >= 10){
+            stack.damage(1, user, (p) -> {
+                p.sendToolBreakStatus(hand);
+            });
+            DurabilityCount = 0;
+        }
+        DurabilityCount ++;
         return ActionResult.PASS;
+    }
+
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        PlayerEntity playerEntity = context.getPlayer();
+        World world = context.getWorld();
+        BlockPos blockPos = context.getBlockPos();
+        BlockState blockState = world.getBlockState(blockPos);
+        if (!CampfireBlock.canBeLit(blockState) && !CandleBlock.canBeLit(blockState) && !CandleCakeBlock.canBeLit(blockState)) {
+            BlockPos blockPos2 = blockPos.offset(context.getSide());
+            if (AbstractFireBlock.canPlaceAt(world, blockPos2, context.getPlayerFacing())) {
+                world.playSound(playerEntity, blockPos2, SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
+                BlockState blockState2 = AbstractFireBlock.getState(world, blockPos2);
+                world.setBlockState(blockPos2, blockState2, 11);
+                world.emitGameEvent(playerEntity, GameEvent.BLOCK_PLACE, blockPos);
+                ItemStack itemStack = context.getStack();
+                if (playerEntity instanceof ServerPlayerEntity) {
+                    Criteria.PLACED_BLOCK.trigger((ServerPlayerEntity)playerEntity, blockPos2, itemStack);
+                    if (DurabilityCount >= 10){
+                        itemStack.damage(1, playerEntity, (p) -> {
+                            p.sendToolBreakStatus(context.getHand());
+                        });
+                        DurabilityCount = 0;
+                    }
+                    DurabilityCount ++;
+                }
+
+                return ActionResult.success(world.isClient());
+            } else {
+                return ActionResult.FAIL;
+            }
+        } else {
+            world.playSound(playerEntity, blockPos, SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
+            world.setBlockState(blockPos, (BlockState)blockState.with(Properties.LIT, true), 11);
+            world.emitGameEvent(playerEntity, GameEvent.BLOCK_CHANGE, blockPos);
+            if (playerEntity != null) {
+                if (DurabilityCount >= 10){
+                    context.getStack().damage(1, playerEntity, (p) -> {
+                        p.sendToolBreakStatus(context.getHand());
+                    });
+                    DurabilityCount = 0;
+                }
+                DurabilityCount ++;
+            }
+
+            return ActionResult.success(world.isClient());
+        }
     }
 
     public int getMaxUseTime(ItemStack stack) {
---
+++ src/main/java/net/S_Rko/rkdungeon/mixin/SeasonsMixin.java
@@ -0,0 +1,12 @@
+package net.S_Rko.rkdungeon.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.world.ServerWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
+@Mixin(MinecraftClient.class)
+public class SeasonsMixin {
+
+}
