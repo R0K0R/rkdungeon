30c91f0665c030dc358d37623eb5a567bed76641
injoy <rkr0k0r@gmail.com>
2022-11-11 21:51:08 +0900

    added tooltip for fire sword

--- src/main/java/net/S_Rko/rkdungeon/item/custom/FireSwordItems.java
+++ src/main/java/net/S_Rko/rkdungeon/item/custom/FireSwordItems.java
@@ -4,6 +4,9 @@ import net.S_Rko.rkdungeon.item.ModItems;
 import net.minecraft.advancement.criterion.Criteria;
 import net.minecraft.block.*;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.item.TooltipData;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
@@ -14,13 +17,15 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.property.Properties;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.UseAction;
+import net.minecraft.text.Text;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.event.GameEvent;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Optional;
 
 public class FireSwordItems extends SwordItem {
 
@@ -51,16 +56,21 @@ public class FireSwordItems extends SwordItem {
     }
 
     public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
-        user.playSound(SoundEvents.ITEM_FIRECHARGE_USE, 1, 1);
-        entity.setFireTicks(10000000);
-        if (DurabilityCount >= 10){
-            stack.damage(1, user, (p) -> {
-                p.sendToolBreakStatus(hand);
-            });
-            DurabilityCount = 0;
+        if (true){
+            user.playSound(SoundEvents.ITEM_FIRECHARGE_USE, 1, 1);
+            entity.setFireTicks(1000000000);
+            if (DurabilityCount >= 10){
+                stack.damage(1, user, (p) -> {
+                    p.sendToolBreakStatus(hand);
+                });
+                DurabilityCount = 0;
+            }
+            DurabilityCount ++;
+            return ActionResult.PASS;
         }
-        DurabilityCount ++;
-        return ActionResult.PASS;
+        else {
+            return ActionResult.FAIL;
+        }
     }
 
     public ActionResult useOnBlock(ItemUsageContext context) {
@@ -68,44 +78,48 @@ public class FireSwordItems extends SwordItem {
         World world = context.getWorld();
         BlockPos blockPos = context.getBlockPos();
         BlockState blockState = world.getBlockState(blockPos);
-        if (!CampfireBlock.canBeLit(blockState) && !CandleBlock.canBeLit(blockState) && !CandleCakeBlock.canBeLit(blockState)) {
-            BlockPos blockPos2 = blockPos.offset(context.getSide());
-            if (AbstractFireBlock.canPlaceAt(world, blockPos2, context.getPlayerFacing())) {
-                world.playSound(playerEntity, blockPos2, SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
-                BlockState blockState2 = AbstractFireBlock.getState(world, blockPos2);
-                world.setBlockState(blockPos2, blockState2, 11);
-                world.emitGameEvent(playerEntity, GameEvent.BLOCK_PLACE, blockPos);
-                ItemStack itemStack = context.getStack();
-                if (playerEntity instanceof ServerPlayerEntity) {
-                    Criteria.PLACED_BLOCK.trigger((ServerPlayerEntity)playerEntity, blockPos2, itemStack);
+        if (true){
+            if (!CampfireBlock.canBeLit(blockState) && !CandleBlock.canBeLit(blockState) && !CandleCakeBlock.canBeLit(blockState)) {
+                BlockPos blockPos2 = blockPos.offset(context.getSide());
+                if (AbstractFireBlock.canPlaceAt(world, blockPos2, context.getPlayerFacing())) {
+                    world.playSound(playerEntity, blockPos2, SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
+                    BlockState blockState2 = AbstractFireBlock.getState(world, blockPos2);
+                    world.setBlockState(blockPos2, blockState2, 11);
+                    world.emitGameEvent(playerEntity, GameEvent.BLOCK_PLACE, blockPos);
+                    ItemStack itemStack = context.getStack();
+                    if (playerEntity instanceof ServerPlayerEntity) {
+                        Criteria.PLACED_BLOCK.trigger((ServerPlayerEntity)playerEntity, blockPos2, itemStack);
+                        if (DurabilityCount >= 10){
+                            itemStack.damage(1, playerEntity, (p) -> {
+                                p.sendToolBreakStatus(context.getHand());
+                            });
+                            DurabilityCount = 0;
+                        }
+                        DurabilityCount ++;
+                    }
+
+                    return ActionResult.success(world.isClient());
+                } else {
+                    return ActionResult.FAIL;
+                }
+            } else {
+                world.playSound(playerEntity, blockPos, SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
+                world.setBlockState(blockPos, blockState.with(Properties.LIT, true), 11);
+                world.emitGameEvent(playerEntity, GameEvent.BLOCK_CHANGE, blockPos);
+                if (playerEntity != null) {
                     if (DurabilityCount >= 10){
-                        itemStack.damage(1, playerEntity, (p) -> {
+                        context.getStack().damage(1, playerEntity, (p) -> {
                             p.sendToolBreakStatus(context.getHand());
                         });
                         DurabilityCount = 0;
                     }
                     DurabilityCount ++;
                 }
-
                 return ActionResult.success(world.isClient());
-            } else {
-                return ActionResult.FAIL;
             }
-        } else {
-            world.playSound(playerEntity, blockPos, SoundEvents.ITEM_FIRECHARGE_USE, SoundCategory.BLOCKS, 1.0F, world.getRandom().nextFloat() * 0.4F + 0.8F);
-            world.setBlockState(blockPos, (BlockState)blockState.with(Properties.LIT, true), 11);
-            world.emitGameEvent(playerEntity, GameEvent.BLOCK_CHANGE, blockPos);
-            if (playerEntity != null) {
-                if (DurabilityCount >= 10){
-                    context.getStack().damage(1, playerEntity, (p) -> {
-                        p.sendToolBreakStatus(context.getHand());
-                    });
-                    DurabilityCount = 0;
-                }
-                DurabilityCount ++;
-            }
-
-            return ActionResult.success(world.isClient());
+        }
+        else{
+            return ActionResult.FAIL;
         }
     }
 
@@ -121,5 +135,18 @@ public class FireSwordItems extends SwordItem {
 
     public boolean canRepair(ItemStack stack, ItemStack ingredient) {
         return ingredient.isOf(ModItems.FIRE_ENERGY);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if(Screen.hasShiftDown()) {
+            tooltip.add(Text.literal("Use on Block: works like flint and steel").formatted(Formatting.AQUA));
+            tooltip.add(Text.literal("Use on Entity: give entity permanent fire(only extinguishable by water)").formatted(Formatting.AQUA));
+            tooltip.add(Text.literal("WARNING: You can't use shield with this item").formatted(Formatting.AQUA));
+        } else {
+            tooltip.add(Text.literal("Press Shift for More Information").formatted(Formatting.YELLOW));
+        }
+
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
